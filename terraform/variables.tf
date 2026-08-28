8dccabb3985af476cb7f8a4ef6697086c31b261d
S Rohan Raju <rohanraju84@gmail.com>
2026-08-28 18:37:11 +0000

    Add private subnets, NAT gateway and route tables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -21,3 +21,9 @@ variable "public_subnet_cidrs" {
   type        = list(string)
   default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
+
+variable "private_subnet_cidrs" {
+  description = "IP ranges for the private subnets"
+  type        = list(string)
+  default     = ["10.0.11.0/24", "10.0.12.0/24"]
+}
